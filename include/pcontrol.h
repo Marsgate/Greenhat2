97e72994ef3bfdb32d74bd8bcaa690aac2a30589
Micah Rassi <micahrassi@gmail.com>
2020-01-15 16:04:24 -0500

    fixed pcontroller

--- include/pcontrol.h
+++ include/pcontrol.h
@@ -3,8 +3,10 @@ using namespace vex;
 
 class Pcontroller{
   double _target;
+  double _lastTarget = 0;
+  double _count = 0;
+  double _last = 0;
   double _kp;
-  double _speed;
   double _margin;
   double _kf;
   motor_group _mg;
@@ -16,12 +18,32 @@ class Pcontroller{
     _margin = margin;
     _kf = kf;
   }
+  bool isMoving(){
+    int curr = _mg.rotation(rotationUnits::deg);
+
+    if(std::abs(_last-curr) < 3)
+      _count++;
+    else
+      _count = 0;
+
+    if(_target != _lastTarget)
+      _count = 0;
+
+    _lastTarget = _target;
+    _last = curr;
+
+    if(_count > 4)
+      return false;
+    else
+      return true;
+  }
   void spin(double target){
     _target = target;
     _mg.spin(fwd, getOutput(), voltageUnits::mV);
   }
   void spinTo(double target){
-    while(std::abs(_mg.position(deg) - target) > _margin){
+    wait(250, msec);
+    while(isMoving()){
       spin(target);
       wait(20, msec);
     }
@@ -30,12 +52,11 @@ class Pcontroller{
   double getOutput(){
     double sv = _mg.position(deg);
     double speed = (_target-sv)*_kp*120;
-    if(speed > 0 ){
-      speed += _kf;
-    }else{
-      speed -= _kf;
+    if(speed > 0 && speed < _kf){
+      speed = _kf;
+    }else if(speed < 0 && speed > _kf){
+      speed = -_kf;
     }
     return speed;
   }
-  
 };
